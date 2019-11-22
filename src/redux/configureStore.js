65a12dbafdef58d3fc95393be99dee86ed815f17
QSD_k <info@qsdbih.com>
2019-11-22 09:44:21 +0100

    Remove unnecessary redux package

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,13 +1,9 @@
 import { createStore, applyMiddleware } from 'redux';
-import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers/index';
 
 const configureStore = () => {
-    return createStore(
-        rootReducer,
-        applyMiddleware(thunk, reduxImmutableStateInvariant())
-    );
+  return createStore(rootReducer, applyMiddleware(thunk));
 };
 
 export default configureStore;
