dd80cd6fe458745fcae6d690e977895d94a060f6
QSD_k <info@qsdbih.com>
2019-12-06 16:03:10 +0100

    Small fix; Line order

--- src/redux/actions/userActions.test.js
+++ src/redux/actions/userActions.test.js
@@ -19,25 +19,25 @@ describe('User actions', () => {
   afterEach(() => moxios.uninstall());
 
   it('should dispatch 2 actions along with USER_ADDED in case of success', async () => {
-    const expectedActions = [ADD_USER_BEGIN, USER_ADDED];
     moxios.stubRequest('https://api.github.com/users/kresimirpehar', {
       status: 200,
       response: {}
     });
     await store.dispatch(addGitHubUser('kresimirpehar'));
     receivedActions = store.getActions().map(action => action.type);
+    const expectedActions = [ADD_USER_BEGIN, USER_ADDED];
     expect(receivedActions).toHaveLength(2);
     expect(receivedActions).toEqual(expectedActions);
   });
 
   it('should dispatch 2 actions along with ADD_USER_ERROR in case of failure', async () => {
-    const expectedActions = [ADD_USER_BEGIN, ADD_USER_ERROR];
     moxios.stubRequest('https://api.github.com/users/kresimirpehar', {
       status: 400,
       response: {}
     });
     await store.dispatch(addGitHubUser('kresimirpehar'));
     receivedActions = store.getActions().map(action => action.type);
+    const expectedActions = [ADD_USER_BEGIN, ADD_USER_ERROR];
     expect(receivedActions).toHaveLength(2);
     expect(receivedActions).toEqual(expectedActions);
   });
