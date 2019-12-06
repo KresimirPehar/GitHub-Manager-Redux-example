8575c178e7e3ab2d0aa758f3b7496f305825f3ab
QSD_k <info@qsdbih.com>
2019-12-06 14:13:33 +0100

    Rename action

--- src/redux/actions/userTypes.js
+++ src/redux/actions/userTypes.js
@@ -1,4 +1,4 @@
 export const ADD_USER_BEGIN = 'ADD_USER_BEGIN';
-export const USER_ADDED = 'ADD_USER';
+export const USER_ADDED = 'USER_ADDED';
 export const ADD_USER_ERROR = 'ADD_USER_ERROR';
 export const FILTER_USER = 'FILTER_USER';
